d96231f7244a508d82e6876642981a5fe0d47cf0
Katherine de Vay <katdevay2@gmail.com>
2022-09-15 13:55:31 -0500

    Finish second draft of calculator js

--- index.js
+++ index.js
@@ -9,94 +9,84 @@ function add(num1, num2){ return num1 + num2; }
 function subtract(num1, num2){ return num1 - num2; }
 
 // divide 
-function divide(num1, num2){
+function divide(num1, num2) {
     if (!num2){
         // division by zero
-        return "ERROR"; 
+        return 'ERROR'; 
     }
     return num1 / num2;
 }
 
 //////////// TRANSFORM FUNCTIONS ////////////
-// Changes sign of existing input number
-// No changes applied on FirstInput
+// Changes sign of operation number & display number
 function changeSign() { 
-    if (isSecondInput || isThirdInput){
-        operation[0] *= -1;
-        answer = operation[0];
+    if (isSecondInput) { // No changes applied on FirstInput
+        answer = parseFloat(operation[0]) * -1;
+        operation[0] = answer + '';
+        return;
+    } else if (isThirdInput){
+        answer = '-';
+        (operation[2]) = '-';
         return;
     }
-    operation[2] *= -1;
-    answer = operation[2];
+    answer = parseFloat(operation[2]) * -1;
+    operation[2] = answer + '';
     return;
 }
 
 // Clear
 function clear(){
-    answer = 0.0;
+    answer = '0.0';
     operation = []
-    changeSignFlag = false;
-    decimalFlag = false;
-    decimalTracker = 10;
-    numSizeTracker = 10;
+    reset();
+    return;
 }
 
-// combines numbers > 9
-function concatNumber(input){
-    if (isSecondInput){
-        input += operation[0] * numSizeTracker;
-        numSizeTracker *= 10
-        return;
-    }
-    input += operation[2] * numSizeTracker;
-    numSizeTracker *= 10
+// reset flags to default values
+function resetFlags(){
+    isAnsFlag = false;
+    decimalAdded = false;
     return;
 }
 
 // decimal
 function addDecimal(input){
-    if (input === ".") {
-        if (isFirstInput) {
-            operation[0] = 0.00000000001;
-            return;
-        } else if (isSecondInput){
-            operation[0] += 0.00000000001;
-            return;
-        } else if (isThirdInput) {
-            operation[2] = .00000000001;
-            return;
-        }
-        operation[2] += 0.00000000001;
-        return;
-    } else { // If input is a number
-        if (isSecondInput){
-            operation[0] += (input / decimalTracker);
-            decimalTracker *= 10;
-        } else { // if isThirdInput
-            operation[2] += (input / decimalTracker)
-            decimalTracker *= 10;
-        }
-        // Add answer to display
-        if (!sizeCheck(operation[2])) {
-            answer = operation[2];
-        } else {
-            answer = sciNotationConverter(operation[2]);
-        }
-        return;
+    if (isFirstInput || isThirdInput) {
+        operation.push('.');
+    } else if (isSecondInput){
+        operation[0] += '.'; 
+    } else {// if isFourthInput 
+        operation[2] += '.';
     }
+    decimalAdded = true; 
+    return;
 }
 
-function sizeCheck(number) {
-    if (number > 9999999999.9 || number < 0.0000000001){
+
+function isTooLong(number) {
+    if (number > 9999999999 || number < 0.00000001){
         return true;
     }
     return false;
 }
 
+
 // Converts to and from scientific notation for extra large and extra small numbers
-function sciNotationConverter(number){
-    // Convert num to string for answer display
-    if (typeof number !== "string"){ 
+function sciNotationConverter(number) {
+    // Convert into long number
+    if (string[3] === 'E') { 
+        // find index of "E"
+        let splitSpot = number.indexOf('E');
+        if (splitSpot === -1) {
+            return "ERROR";
+        }
+        // split string in two, omitting e & cast as float
+        let a = parseFloat(number.slice(0, splitSpot));
+        let b = parseFloat(number.slice(splitSpot + 1));
+        // first half of string * (10 ** second half of string)
+        return a * (10 ** b);
+    // Convert into scientific notation
+    } else { 
         let exponent = 0;
         if (number < 1){ // If very small
             while (num < 1){
@@ -109,19 +99,8 @@ function sciNotationConverter(number){
                 num = num / 10;
             }
         }
-        return num + "E" + exponent;
-    // Convert string to num for calculations
-    } else { 
-        // find index of "E"
-        let splitSpot = number.indexOf('E');
-        if (splitSpot === -1){
-            return "ERROR";
-        }
-        // split string in two, omitting e & cast as float
-        let a = parseFloat(number.slice(0, splitSpot));
-        let b = parseFloat(number.slice(splitSpot + 1));
-        // first half of string * (10 ** second half of string)
-        return a * (10 ** b);
+    isDecimalFlag = true;
+    return num + "E" + exponent;
     }
 }
 
@@ -131,83 +110,82 @@ function sciNotationConverter(number){
 function operationLimiter(input){
     // Decimals //
     if (input === '.') {
-        if (!decimalFlag){
+        if (!decimalAdded){
                 addDecimal();
-                decimalFlag = true;
                 return;
         }
-        return "ERROR";
+        // Don't allow multiple decimals in one number
+        return "ERROR"; 
+    }
+
+    // Transformations //
+    if (input === 'sign-change') { // sign change
+        changeSign();
+        return;
+    } else if (input === 'clear'){ // clear
+        clear();
+        return;
     }
 
     // Operators //
     if (input === isOperator) { 
-        if (decimalFlag === true) {
-            decimalFlag = false;
-        } else if (isFirstInput) { // default to zero if 1st input is operator
+        resetFlags();
+        if (isFirstInput) { // default to zero if 1st input is operator
             operation.push(0);
+        } else if(isFourthInput){
+            let temp = calculate(operation);
+            operation = [];
+            operation.push(temp);
         }
-        operation.push(input);// -------------------------------------
-        return;
-
-    // Transformations //
-    } else if (typeof input === "string"){
-        if (input === 'sign-change') { // sign change
-            changeSign();
-            return;
-        } else if (input === 'clear'){ // clear
-            clear();
-            return;
-        } else { // convert to number
-            input = sciNotationConverter(input);
-        }
+    }
 
     // Numbers //
-    } else if (typeof input === 'number') {
-        if (decimalFlag === true){
-            addDecimal(input);
+    if ( input === isNumber) {
+        if (isSecondInput){
+            operation[0] += input;
+            answer = input;
             return;
-        } 
-        if (isSecondInput || isFourthInput){
-            concatNumber(input);
-            return
+        } else if (isFourthInput){
+            operation[2] += input;
+            answer = input;
+            return;
         }
-
-
-
-    answer = "INTERNAL ERROR";
+    }
+    operation.push(input);
     return;
-
-    // answer = input;
-    // operation.push(input);
-    // return;
 }
 
 
 
-
-
-
-
-
-
-
 // calculator
-function calculate(array){
-    let num1 = array[0];
-    let num2 = array[2];
+function calculate(array) {
+    let num1 = parseFloat(array[0]); // Ensure numbers are floats
+    let num2 = parseFloat(array[2]);
     let action = array[1];
     let ans = 0;
-    // Ensure number is float
-    console.log("num1: ",num1);
-    console.log("num2: ", num2);
-    console.log("action: ", action);
-    return;
-    // logic for finding operator
-    // call appropriate helper function
-    // check final number for size
-    // if (ans > 9999999999.9){
-    //     return sciNotationConverter(answer);
-    // }
+    
+    // Find which operator is being called
+    if (action = '+') {
+        ans += add(num1, num2);
+    } else if (action = '-') {
+        ans += subtract(num1, num2);
+    } else if (action = '/' {
+        ans += divide(num1, num2);
+    } else { // action = '*'
+        ans += multiply(num1, num2);
+    }
+
+    // Flag if ans contains decimal
+    let temp = ans + '';
+    if (temp.indexOf('.') !== -1) {
+        decimalAdded = true;
+    }
+    // Ensure number fits display
+    if (isTooLong(ans)){
+        answer = sciNotationConverter(ans);
+    }
+    answer = ans;
+    return temp;
 }
 
 // Operation chain
@@ -218,25 +196,25 @@ let answer = document.getElementById('answer').textContent;
 
 // Number buttons
 const zero = document.getElementById('0');
-zero.addEventListener('click', function(){ operationLimiter(0)});
+zero.addEventListener('click', function(){ operationLimiter('0')});
 const one = document.getElementById('1');
-one.addEventListener('click', function(){ operationLimiter(1)});
+one.addEventListener('click', function(){ operationLimiter('1')});
 const two = document.getElementById('2');
-two.addEventListener('click', function(){ operationLimiter(2)});
+two.addEventListener('click', function(){ operationLimiter('2')});
 const three = document.getElementById('3');
-three.addEventListener('click', function(){ operationLimiter(3)});
+three.addEventListener('click', function(){ operationLimiter('3')});
 const four = document.getElementById('4');
-four.addEventListener('click', function(){ operationLimiter(4)});
+four.addEventListener('click', function(){ operationLimiter('4')});
 const five = document.getElementById('5');
-five.addEventListener('click', function(){ operationLimiter(5)});
+five.addEventListener('click', function(){ operationLimiter('5')});
 const six = document.getElementById('6');
-six.addEventListener('click', function(){ operationLimiter(6)});
+six.addEventListener('click', function(){ operationLimiter('6')});
 const seven = document.getElementById('7');
-seven.addEventListener('click', function(){ operationLimiter(7)});
+seven.addEventListener('click', function(){ operationLimiter('7')});
 const eight = document.getElementById('8');
-eight.addEventListener('click', function(){ operationLimiter(8)});
+eight.addEventListener('click', function(){ operationLimiter('8')});
 const nine = document.getElementById('9');
-nine.addEventListener('click', function(){ operationLimiter(9)});
+nine.addEventListener('click', function(){ operationLimiter('9')});
 
 // Operator buttons
 const dividedBy = document.getElementById('divide');
@@ -250,19 +228,20 @@ plus.addEventListener('click', function(){ operationLimiter('+')});
 
 // Transform buttons
 const signChange = document.getElementById('sign-change');
-// nine.addEventListener('click', function(){ operationLimiter(9)});
+signChange.addEventListener('click', function(){ operationLimiter('sign-change')});
 const clear = document.getElementById('clear');
-// nine.addEventListener('click', function(){ operationLimiter(9)});
+clear.addEventListener('click', function(){ operationLimiter('clear')});
 const decimal = document.getElementById('.')
-// nine.addEventListener('click', function(){ operationLimiter(9)});
+decimal.addEventListener('click', function(){ operationLimiter('decimal')});
 
 // Bools
 const isOperator = ('/' || '*' || '-' || '+');
+const isNumber = ('0' || '1' || '2' || '3' || '4' || '5' || '6' || '7' || '8' || '9');
 const isFirstInput = operation.length === 0;
 const isSecondInput = operation.length === 1;
 const isThirdInput = operation.length === 2;
 const isFourthInput = operation.length === 3;
-let changeSignFlag = false;
-let decimalFlag = false;
-let decimalTracker = 10;
-let numSizeTracker = 10;
+
+// Enabled if '.' is input or calculation results in decimal
+// Disabled if operator or '=' is input
+let decimalAdded = false; 
